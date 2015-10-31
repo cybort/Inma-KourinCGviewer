13c59b95794161a45ddced10926d0073b26c2373
huazhu111 <luodadi.huazhu@gmail.com>
2015-10-31 12:01:21 +0800

    changes

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(33, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox1_LoadCompleted);
+            this.pictureBox1.LoadProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.pictureBox1_LoadProgressChanged);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(857, 105);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox2_LoadCompleted);
+            this.pictureBox2.LoadProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.pictureBox2_LoadProgressChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "上一张";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(803, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "下一张";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.ImageLocation = "";
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(700, 525);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox4_LoadCompleted);
+            this.pictureBox4.LoadProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.pictureBox4_LoadProgressChanged);
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(262, 737);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(323, 41);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 8;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(1172, 723);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(323, 41);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 9;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(209, 1022);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(323, 41);
+            this.progressBar3.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(304, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "true";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(502, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 46);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "确定";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Location = new System.Drawing.Point(607, 781);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 525);
+            this.panel1.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 45;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(400, 933);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 52);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "加载动画";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1675, 105);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(465, 620);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(1675, 737);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(465, 620);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2152, 1398);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Inma-KourinCGviewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        int count = 0;
+        int animecount = 1;
+        bool pic1 = true;
+        bool pic2 = true;
+        bool pic3 = true;
+        string url1 = "";
+        string url2 = "";
+        string url3 = "";
+        string url4 = "";
+        string url5 = "";
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ShowPic(textBox1.Text);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            
+            {
+                textBox1.Text = ((int.Parse(textBox1.Text))-4).ToString();
+                ShowPic(textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.Text = ((int.Parse(textBox1.Text)) + 4).ToString();
+                ShowPic(textBox1.Text);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowPic(string id)
+        {
+            id = (int.Parse(id)+300000).ToString();
+            pictureBox1.ImageLocation = "";
+            pictureBox2.ImageLocation = "";
+            pictureBox1.CancelAsync();
+            pictureBox2.CancelAsync();
+            pictureBox4.CancelAsync();
+            string sid = (int.Parse(id) + 1000).ToString();
+            url1 = string.Format("http://157.7.147.219/img/anime2/sm_99_{0}_sean1.jpg",id);
+            url2 = string.Format("http://157.7.147.219/img/anime2/sm_99_{0}_sean3.jpg", id);
+            url3 = string.Format("http://157.7.147.219/img/anime2/sm_99_{0}_sean2.jpg", id);
+            url4 = string.Format("http://157.7.147.219/img/card/de_card_e_{0}.jpg", sid);
+            url5 = string.Format("http://157.7.147.219/img/card/de_card_e_{0}.jpg", id);
+            if (pic1 == true)
+            {
+                pic1 = false;
+                label1.Text = "false";
+                pictureBox1.ImageLocation = @url1;
+                
+            }
+            if (pic2 == true)
+            {
+                pic2 = false;
+                pictureBox2.ImageLocation = @url2;
+            }
+            pictureBox3.ImageLocation = @url4;
+            pictureBox5.ImageLocation = @url5;
+        }
+
+        private void pictureBox2_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                pic2 = true;
+            }
+            else pic2 = true;
+        }
+
+        private void pictureBox2_LoadProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar2.Value = e.ProgressPercentage;
+
+        }
+
+        private void pictureBox1_LoadProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            if (e.UserState != null)
+            {
+                MessageBox.Show(e.UserState.ToString());
+            }
+        }
+
+        private void pictureBox4_LoadProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar3.Value = e.ProgressPercentage;
+        }
+
+        private void pictureBox4_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                string id = textBox1.Text;
+                id = (int.Parse(id) + 300000).ToString();
+                string url = String.Format("http://157.7.147.219/img/anime2/sm_99_{0}_sean2_1.jpg", id);
+                pictureBox4.ImageLocation = @url;
+            }
+            if (e.Cancelled == true)
+            {
+                pic3 = true;
+            }
+            else
+            {
+                pic3 = true;
+                animecount = pictureBox4.Image.Size.Height / (pictureBox4.Image.Size.Width / 4 * 3);
+                pictureBox4.Height = pictureBox4.Width / 4 * 3 * animecount;
+            }
+        }
+
+        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                pic1 = true;
+                label1.Text = "true";
+             //   MessageBox.Show("取消啦");
+            }
+            else { pic1 = true; label1.Text = "true"; }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShowPic(textBox1.Text);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled == false) timer1.Enabled = true;
+            else
+            {
+                timer1.Enabled = false;
+                pictureBox4.Top = 0;
+                count = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if (count == animecount) { pictureBox4.Top = 0; count = 0; }
+            else pictureBox4.Top -= pictureBox4.Height/animecount;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            pictureBox4.Top = 0;
+            count = 0;
+            if (pic3 == true)
+            {
+                pic3 = false;
+                pictureBox4.Height = pictureBox4.Width / 4 * 3;
+                pictureBox4.ImageLocation = @url3;
+            }
+        }
+    }
+}
